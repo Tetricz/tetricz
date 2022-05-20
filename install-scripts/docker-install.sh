9b586ed54d9b8614e9edffcf927370e76659042b
Tetricz <DavidLeeDaniels@protonmail.com>
2022-05-19 20:56:41 -0500

    add docker install script with user post install

---
+++ install-scripts/docker-install.sh
@@ -0,0 +1,8 @@
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install docker.io
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
+echo "Testing docker with hello-world"
+docker run hello-world
