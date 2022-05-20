133afa0ce942bc51e2e99f09ea68f907b04406b8
Tetricz <DavidLeeDaniels@protonmail.com>
2022-05-20 15:28:41 -0500

    add remove containers scripts

--- install-scripts/docker-install.sh
+++ install-scripts/docker-install.sh
@@ -4,6 +4,4 @@ sudo apt-get upgrade -y
 sudo apt-get install docker.io
 sudo groupadd docker
 sudo usermod -aG docker $USER
-newgrp docker
-echo "Testing docker with hello-world"
-docker run hello-world
+echo "Logout and login"
