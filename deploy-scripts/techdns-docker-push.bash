d6800b63a285cba4e70796352bfdab3296ea4bc0
Tetricz <DavidLeeDaniels@protonmail.com>
2022-05-20 20:43:27 -0500

    change runtime version

--- deploy-scripts/techdns-docker-push.bash
+++ deploy-scripts/techdns-docker-push.bash
@@ -9,8 +9,8 @@ echo "Enable multi-architecture support"
 docker run --privileged --rm docker/binfmt:a7996909642ee92942dcd6cff44b9b95f08dad64
 echo "Starting build process"
 docker buildx build --build-arg SDK_VERSION=6.0 --build-arg RUNTIME_VERSION=6.0-alpine3.15-amd64 --platform linux/amd64 -f "Dockerfile" -t tetricz/techdns:amd64$timevar --no-cache .
-docker buildx build --build-arg SDK_VERSION=6.0 --build-arg RUNTIME_VERSION=6.0-alpine3.15-arm64v8 --platform linux/arm64 -f "Dockerfile" -t tetricz/techdns:arm64$timevar --no-cache .
-docker buildx build --build-arg SDK_VERSION=6.0 --build-arg RUNTIME_VERSION=6.0-bullseye-slim-arm32v7 --platform linux/arm/v7 -f "Dockerfile" -t tetricz/techdns:armv7$timevar --no-cache .
+docker buildx build --build-arg SDK_VERSION=6.0 --build-arg RUNTIME_VERSION=6.0 --platform linux/arm64 -f "Dockerfile" -t tetricz/techdns:arm64$timevar --no-cache .
+docker buildx build --build-arg SDK_VERSION=6.0 --build-arg RUNTIME_VERSION=6.0 --platform linux/arm/v7 -f "Dockerfile" -t tetricz/techdns:armv7$timevar --no-cache .
 echo "Pushing images to DockerHub"
 docker push tetricz/techdns:amd64$timevar
 docker push tetricz/techdns:arm64$timevar
