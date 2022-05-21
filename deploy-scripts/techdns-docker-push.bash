5dfcb5312706569f34954c9a14fad309ec8277dd
Tetricz <DavidLeeDaniels@protonmail.com>
2022-05-20 21:18:32 -0500

    remove arm32

--- deploy-scripts/techdns-docker-push.bash
+++ deploy-scripts/techdns-docker-push.bash
@@ -10,12 +10,12 @@ docker run --privileged --rm docker/binfmt:a7996909642ee92942dcd6cff44b9b95f08da
 echo "Starting build process"
 docker buildx build --build-arg SDK_VERSION=6.0 --build-arg RUNTIME_VERSION=6.0-alpine3.15-amd64 --platform linux/amd64 -f "Dockerfile" -t tetricz/techdns:amd64$timevar --no-cache .
 docker buildx build --build-arg SDK_VERSION=6.0 --build-arg RUNTIME_VERSION=6.0 --platform linux/arm64 -f "Dockerfile" -t tetricz/techdns:arm64$timevar --no-cache .
-docker buildx build --build-arg SDK_VERSION=6.0 --build-arg RUNTIME_VERSION=6.0 --platform linux/arm/v7 -f "Dockerfile" -t tetricz/techdns:armv7$timevar --no-cache .
+#docker buildx build --build-arg SDK_VERSION=6.0 --build-arg RUNTIME_VERSION=6.0 --platform linux/arm/v7 -f "Dockerfile" -t tetricz/techdns:armv7$timevar --no-cache .
 echo "Pushing images to DockerHub"
 docker push tetricz/techdns:amd64$timevar
 docker push tetricz/techdns:arm64$timevar
-#docker push tetricz/techdns:armv7$timevar
-docker manifest create tetricz/techdns:latest tetricz/techdns:arm64$timevar tetricz/techdns:amd64$timevar tetricz/techdns:armv7$timevar --amend
+#docker push tetricz/techdns:armv7$timevar tetricz/techdns:armv7$timevar
+docker manifest create tetricz/techdns:latest tetricz/techdns:arm64$timevar tetricz/techdns:amd64$timevar --amend
 docker manifest push --purge tetricz/techdns:latest
 echo "Cleaning up files"
 docker buildx prune -fa
