b4bb3d57f83f6eaf019f4cb0c52a732d105f3305
Tetricz <DavidLeeDaniels@protonmail.com>
2022-06-12 02:50:41 -0500

    refactor to one

--- gitpull-forced.sh
+++ gitpull-forced.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+echo "Resetting for git pull"
+git reset --hard
+git pull
 echo "Making all scripts execuatable"
 chmod -R +x ./*
--- gitpull.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Resetting for git pull"
-git reset --hard
-git pull
-chmod +x ./exec.sh
-./exec.sh
