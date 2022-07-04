1b612cac1c1333956bc983eaa45554cfd8be6905
Tetricz <DavidLeeDaniels@protonmail.com>
2022-07-04 06:02:18 -0500

    remove alpine

--- deploy-scripts/techdns-docker-push.bash
+++ deploy-scripts/techdns-docker-push.bash
@@ -8,7 +8,7 @@ cd docker-techdns
 echo "Enable multi-architecture support"
 docker run --privileged --rm docker/binfmt:a7996909642ee92942dcd6cff44b9b95f08dad64
 echo "Starting build process"
-docker buildx build --build-arg SDK_VERSION=6.0-alpine --build-arg RUNTIME_VERSION=6.0-alpine --platform linux/amd64 -f "Dockerfile" -t tetricz/techdns:amd64$timevar --no-cache .
+docker buildx build --build-arg SDK_VERSION=6.0-alpine --build-arg RUNTIME_VERSION=6.0 --platform linux/amd64 -f "Dockerfile" -t tetricz/techdns:amd64$timevar --no-cache .
 docker buildx build --build-arg SDK_VERSION=6.0 --build-arg RUNTIME_VERSION=6.0 --platform linux/arm64 -f "Dockerfile" -t tetricz/techdns:arm64$timevar --no-cache .
 #docker buildx build --build-arg SDK_VERSION=6.0 --build-arg RUNTIME_VERSION=6.0 --platform linux/arm/v7 -f "Dockerfile" -t tetricz/techdns:arm32$timevar --no-cache .
 echo "Pushing images to DockerHub"
