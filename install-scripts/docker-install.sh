637c31d2726c18775c6b8fcf8dd345cc3663b864
Tetricz <DavidLeeDaniels@protonmail.com>
2022-09-15 01:02:10 -0500

    update to use docker repo

--- install-scripts/docker-install.sh
+++ install-scripts/docker-install.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install docker.io
-sudo groupadd docker
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+# Add Docker’s official GPG key:
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+
+# Add user to docker group
 sudo usermod -aG docker $USER
-echo "Logout and login"
+echo "Please log out and log back in to apply changes or reload shell"
