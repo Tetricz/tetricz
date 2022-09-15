2186501a39a171365ac0bb79a05e08dfd158c51d
Tetricz <DavidLeeDaniels@protonmail.com>
2022-09-15 05:01:55 -0500

    typo

--- deploy-scripts/se7encracker.sh
+++ deploy-scripts/se7encracker.sh
@@ -4,7 +4,7 @@ echo "Time Variable set to ${timevar}"
 echo "Downloading repository"
 git clone -4 https://github.com/Tetricz/se7enCracker.git
 echo "Entering repository"
-cd se7encracker
+cd se7enCracker
 git checkout dev
 echo "Enable multi-architecture support"
 docker run --privileged --rm docker/binfmt:a7996909642ee92942dcd6cff44b9b95f08dad64
@@ -17,4 +17,4 @@ docker manifest push --purge tetricz/se7encracker:latest
 echo "Cleaning up files"
 docker buildx prune -fa
 cd ..
-rm -vfr se7encracker
+rm -vfr se7enCracker
